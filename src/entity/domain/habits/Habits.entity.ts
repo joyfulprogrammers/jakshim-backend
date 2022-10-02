b9b5867716184b38fd7427c78abf0f8a325e8019
Moonki Lee <shinbaek89@gmail.com>
2022-10-02 10:07:35 +0900

    Habits Entity 컬럼 이름 변경 및 설명 추가

--- src/entity/domain/habits/Habits.entity.ts
+++ src/entity/domain/habits/Habits.entity.ts
@@ -12,51 +12,54 @@ export class Habits extends BaseTimeEntity {
   @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
   userId: number;
 
-  @Column()
+  @Column({ comment: '습관 이름' })
   name: string;
 
-  @Column()
-  color: string;
+  @Column({ comment: '습관 테마 색상' })
+  theme_color: string;
 
-  @Column()
+  @Column({ comment: '습관 폰트 색상' })
   fontColor: string;
 
-  @Column()
-  imageUrl: string;
+  @Column({ comment: '습관 아이콘 이미지' })
+  iconImageUrl: string;
 
-  @Column()
+  @Column({ comment: '습관 달성 기준 횟수' })
   targetCount: number;
 
-  @Column()
+  @Column({ comment: '습관 타입' })
   type: HabitType;
 
-  @Column()
+  @Column({
+    comment:
+      '중요 습관이면, 달성 주기에 해당하는 날을 ‘달성한 날’로 체크하는 데 기준이 됨',
+  })
   isImportant: boolean;
 
-  @Column()
+  @Column({ comment: '주기에 월요일 포함 여부' })
   cycleMonday: boolean;
 
-  @Column()
+  @Column({ comment: '주기에 화요일 포함 여부' })
   cycleTuesday: boolean;
 
-  @Column()
+  @Column({ comment: '주기에 수요일 포함 여부' })
   cycleWednesday: boolean;
 
-  @Column()
+  @Column({ comment: '주기에 목요일 포함 여부' })
   cycleThursday: boolean;
 
-  @Column()
+  @Column({ comment: '주기에 금요일 포함 여부' })
   cycleFriday: boolean;
 
-  @Column()
+  @Column({ comment: '주기에 토요일 포함 여부' })
   cycleSaturday: boolean;
 
-  @Column()
+  @Column({ comment: '주기에 일요일 포함 여부' })
   cycleSunday: boolean;
 
-  @Column()
+  @Column({ comment: '주기가 일주일인지 여부' })
   cycleWeek: boolean;
 
-  @Column()
+  @Column({ comment: '주기가 한 달인지 여부' })
   cycleMonth: boolean;
 }
