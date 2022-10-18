ef8b123c6819b4759b1b0a2ff44512f30d0fc502
Moonki Lee <shinbaek89@gmail.com>
2022-10-18 23:17:58 +0900

    사용자 닉네임을 통한 회원가입 구현 (테스트 X)

--- src/module/user/UserController.ts
+++ src/module/user/UserController.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserSaveRequest } from './dto/UserSaveRequest';
+import { UserService } from './UserService';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('/signup')
+  async save(@Body() req: UserSaveRequest) {
+    await this.userService.save(req);
+  }
+}
--- src/module/user/UserService.ts
+++ src/module/user/UserService.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entity/domain/user/User.entity';
+import { UserSaveRequest } from './dto/UserSaveRequest';
+import { UserRepository } from './repository/UserRepository';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: UserRepository,
+  ) {}
+
+  async save(dto: UserSaveRequest) {
+    await this.userRepository.save(dto);
+  }
+}
