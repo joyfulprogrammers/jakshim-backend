1da07dde4e8a8ed6e16e105ee9681fb98333161d
Junep <prepared@inflearn.com>
2022-10-26 09:14:11 +0900

    synchronizeEntities를 개선
    기존 방법으로 하니 테이블의 데이터들이 제거되는 현상이 있어서 MikroORM을 통해 제너레이터를 사용하려고 했습니다.
    MikroORM.init을 사용하려 했으나 몇몇 테이블이 누락되는 문제가 발생했습니다.
    그래서 문서를 참고하여safe 옵션값을 전달하는 방식으로 해결했습니다.

--- src/libs/synchronizeEntities.ts
+++ src/libs/synchronizeEntities.ts
@@ -1,12 +1,13 @@
 import { MikroORM } from '@mikro-orm/core';
+import config from '../config/mikro-orm.config';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { Test, TestingModule } from '@nestjs/testing';
+import { TestingModule, Test } from '@nestjs/testing';
 import { AchievementEntityModule } from '../entity/domain/achievement/AchievementEntityModule';
 import { HabitEntityModule } from '../entity/domain/habit/HabitEntityModule';
 import { UserEntityModule } from '../entity/domain/user/UserEntityModule';
-import config from '../config/mikro-orm.config';
 
 export const synchronizeEntities = async (): Promise<void> => {
+  // MikroORM.init()을 사용하면, createSchema를 했을때 몇몇 엔티티가 누락되는 문제가 발생
   const module: TestingModule = await Test.createTestingModule({
     imports: [
       MikroOrmModule.forRoot(config),
@@ -15,8 +16,10 @@ export const synchronizeEntities = async (): Promise<void> => {
       AchievementEntityModule,
     ],
   }).compile();
+
   const orm = module.get(MikroORM);
-  await orm.getSchemaGenerator().refreshDatabase();
+  const generator = orm.getSchemaGenerator();
+  await generator.updateSchema({ safe: true });
   await orm.close(true);
 
   return;
