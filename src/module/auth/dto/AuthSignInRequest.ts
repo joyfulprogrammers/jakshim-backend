f255923b65d36a5eb7ac395047265232308059ed
Junep <prepared@inflearn.com>
2022-11-05 23:27:44 +0900

    AuthLocalGuard 문제 수정

--- src/module/auth/dto/AuthSignInRequest.ts
+++ src/module/auth/dto/AuthSignInRequest.ts
@@ -1,8 +1,7 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { Trim } from '../../../decorator/Trim';
 
 export class AuthSignInRequest {
-  @IsEmail()
   @IsString()
   @IsNotEmpty()
   @Trim()
