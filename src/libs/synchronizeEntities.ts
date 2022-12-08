2df4b2c9492552ab2b4f7e6d79945e0d731f0d08
Junep <prepared@inflearn.com>
2022-12-08 21:20:15 +0900

    Migration programmatically  시도

--- src/libs/synchronizeEntities.ts
+++ src/libs/synchronizeEntities.ts
@@ -20,6 +20,15 @@ export const synchronizeEntities = async (): Promise<void> => {
   const orm = module.get(MikroORM);
   const generator = orm.getSchemaGenerator();
   await generator.updateSchema({ safe: true });
+
+  // const migrator = orm.getMigrator();
+  // try {
+  //   await migrator.createMigration();
+  //   await migrator.up();
+  // } catch (error) {
+  //   await migrator.down();
+  // }
+
   await orm.close(true);
 
   return;
