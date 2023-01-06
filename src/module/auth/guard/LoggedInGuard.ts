91668bde467eeb285d1ac0734ecaac7ed6add495
Molly <dngwoodo@gmail.com>
2023-01-07 00:04:51 +0900

    feat: LoggedInGuard, NotLoggedInGuard 구현

---
+++ src/module/auth/guard/LoggedInGuard.ts
@@ -0,0 +1,22 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class LoggedInGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    if (!request.isAuthenticated()) {
+      throw new UnauthorizedException();
+    }
+
+    return request.isAuthenticated();
+  }
+}
