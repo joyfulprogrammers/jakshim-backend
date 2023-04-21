daacdee1a4602c6e144f5dc61dc5096f6e88055f
minwoo <mwoodev526@gmail.com>
2023-04-21 20:09:29 +0900

    사용하지 않은 코드 주석 처리

--- src/module/badhabit/BadhabitQueryRepository.ts
+++ src/module/badhabit/BadhabitQueryRepository.ts
@@ -1,12 +1,6 @@
-import { InjectRepository } from '@mikro-orm/nestjs';
-import { EntityRepository } from '@mikro-orm/postgresql';
 import { Injectable } from '@nestjs/common';
-import { Badhabit } from 'src/entity/domain/badhabit/Badhabit.entity';
 
 @Injectable()
 export class BadhabitQueryRepository {
-  constructor(
-    @InjectRepository(Badhabit)
-    private readonly badhabitRepository: EntityRepository<Badhabit>,
-  ) {}
+  constructor() {} // private readonly badhabitRepository: EntityRepository<Badhabit>, // @InjectRepository(Badhabit)
 }
